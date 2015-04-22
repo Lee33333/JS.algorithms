dfab3adcb64fa8092593b0d3587fcb39a7b7b23e
Daria Garina <daria33@gmail.com>
2015-04-22 09:18:30 -0700

    js selection sort

---
+++ js_selection_sort.js
@@ -0,0 +1,32 @@
+var swap = function(array, firstIndex, secondIndex) {
+var temp = array[firstIndex];
+array[firstIndex] = array[secondIndex];
+array[secondIndex] = temp;
+
+};
+
+var indexOfMinimum = function(array, startIndex) { 
+var minValue = array[startIndex];
+var minIndex = startIndex;
+
+for (var i = startIndex + 1; i < array.length; i++){
+    if(array[i] < minValue) {
+        minIndex = i;
+        minValue = array[i];
+        }
+    }
+    return minIndex;
+}; 
+
+var selectionSort = function(array) {
+    var small;
+    for (var i=0; i < array.length; i++) {
+    small = indexOfMinimum(array, i);
+    swap(array, small, i);
+    }
+
+};
+
+var array = [22, 11, 99, 88, 9, 7, 42];
+selectionSort(array);
+console.log("Array after sorting:  " + array);
